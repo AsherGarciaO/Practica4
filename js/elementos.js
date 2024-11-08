8f6aee1a1c467bf6b8ed005dd98fc15a61d60bfd
AsherGarciaO <asherg.o@outlook.com>
2024-11-08 11:15:32 -0600

    Agregado

--- js/elementos.js
+++ js/elementos.js
@@ -1,4 +1,57 @@
 function getValue(id){
-    return document.getElementById(id).value;
+    return parseFloat(document.getElementById(id).value);
 }
 
+function agregarEventos(...ids){
+    ids.forEach(item =>{
+        if(item !== "calcular"){
+            document.getElementById(item).addEventListener('change', ()=>{
+                iniciar();
+            });
+        }
+        else{
+            document.getElementById(item).addEventListener('click', ()=>{
+                iniciar();
+            });
+        }
+    });
+};
+
+function simular(resultados){
+    let plano = document.getElementById("plano");
+    let objeto = document.getElementById("objeto");
+    let flecha = document.getElementById("flecha");
+    let fNeta1 = resultados.f - resultados.peso*sen(resultados.angulo) - resultados.f1;
+    let fNeta2 = resultados.f - resultados.peso*sen(resultados.angulo) - resultados.f2;
+    let fNeta3 = resultados.f - resultados.peso*sen(resultados.angulo) - resultados.f3;
+    let resultado = {r1: fNeta1 > 0, r2: fNeta2 > 0, r3: fNeta3 > 0, f1: fNeta1, f2: fNeta2, f3: fNeta3};
+
+    plano.style.transform = `rotate(${-resultados.angulo}deg)`;
+    console.log(fNeta1);
+    if (fNeta1 > 0) {
+        objeto.style.left = "100px";
+    } 
+    else {
+        objeto.style.left = "50px";
+    }
+
+    if (fNeta1 > 0) {
+        flecha.style.width = `${Math.min(100, Math.max(20, resultados.f))}px`;
+        flecha.style.transform = `rotate(${-resultados.angulo/30}deg)`;
+    } 
+    else {
+        flecha.style.width = "0px";
+    }
+
+    return resultado;
+}
+
+function actualizarResultados(results){
+    document.getElementById("rCF1").textContent = `Coeficiente de Fricción 1: ${(results.r1)?"Sí":"No"}. Fuerza Neta (Reflejada): ${(results.f1>=0)?results.f1+" N":"No válido"}`;
+    document.getElementById("rCF2").textContent = `Coeficiente de Fricción 2: ${(results.r2)?"Sí":"No"}. Fuerza Neta (Reflejada): ${(results.f2>=0)?results.f2+" N":"No válido"}`;
+    document.getElementById("rCF3").textContent = `Coeficiente de Fricción 3: ${(results.r3)?"Sí":"No"}. Fuerza Neta (Reflejada): ${(results.f3>=0)?results.f3+" N":"No válido"}`;
+}
+
+function iniciar(){
+    actualizarResultados(simular(calcularFuerzas(getValue("peso"), getValue("cFriccion1"), getValue("cFriccion2"), getValue("cFriccion3"), getValue("angulo"), getValue("fuerza"))));
+}
--- js/formulas.js
+++ js/formulas.js
@@ -1,4 +1,21 @@
+function calcularFuerzas(peso, ff1, ff2, ff3, angulo, fuerza){
+    let resultados = {n: 0, f1: 0, f2:0, f3:0, f: 0, angulo: 0, peso: 0};
 
+    let normal = (peso*9.81)/(2*sen(angulo));
+    let fFriccion1 = ff1*normal;
+    let fFriccion2 = ff2*normal;
+    let fFriccion3 = ff3*normal;
+
+    resultados.f1 = fFriccion1;
+    resultados.f2 = fFriccion2;
+    resultados.f3 = fFriccion3;
+    resultados.f = fuerza;
+    resultados.n = normal;
+    resultados.angulo = angulo;
+    resultados.peso = peso;
+    
+    return resultados;
+}
 
 function gradosRadianes(grados){
     return (grados * (Math.PI / 180));
